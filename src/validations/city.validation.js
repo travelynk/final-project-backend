5ea32539cabb2c0a54c5a2f953de674a241d0ad9
fajrCode <fajri42vandi@gmail.com>
2024-11-26 15:13:44 +0700

    feat: city validation | set custom error message

--- src/validations/city.validation.js
+++ src/validations/city.validation.js
@@ -1,12 +1,35 @@
 import joi from 'joi';
 
 export const createPayload = joi.object({
-    code: joi.string().min(2).max(4).required(),
-    name: joi.string().max(50).required(),
-    countryCode: joi.string().min(2).max(4).required(),
+    code: joi.string().min(2).max(4).required().messages({
+        'string.base': 'Code should be a type of text',
+        'string.min': 'Code should have a minimum length of 2',
+        'string.max': 'Code should have a maximum length of 4',
+        'any.required': 'Code is a required field'
+    }),
+    name: joi.string().max(50).required().messages({
+        'string.base': 'Name should be a type of text',
+        'string.max': 'Name should have a maximum length of 50',
+        'any.required': 'Name is a required field'
+    }),
+    countryCode: joi.string().min(2).max(4).required().messages({
+        'string.base': 'Country Code should be a type of text',
+        'string.min': 'Country Code should have a minimum length of 2',
+        'string.max': 'Country Code should have a maximum length of 4',
+        'any.required': 'Country Code is a required field'
+    }),
 });
 
 export const updatePayload = joi.object({
-    name: joi.string().max(50).required(),
-    countryCode: joi.string().min(2).max(4).required(),
+    name: joi.string().max(50).required().messages({
+        'string.base': 'Name should be a type of text',
+        'string.max': 'Name should have a maximum length of 50',
+        'any.required': 'Name is a required field'
+    }),
+    countryCode: joi.string().min(2).max(4).required().messages({
+        'string.base': 'Country Code should be a type of text',
+        'string.min': 'Country Code should have a minimum length of 2',
+        'string.max': 'Country Code should have a maximum length of 4',
+        'any.required': 'Country Code is a required field'
+    }),
 });

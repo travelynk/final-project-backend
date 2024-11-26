4c8240263c87de1748e58d295604f06001cef227
fajrCode <fajri42vandi@gmail.com>
2024-11-26 10:16:21 +0700

    chore: update middleeware error handler | add error 404

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 import * as response from '../utils/response.js';
-import { Error400 } from '../utils/customError.js';
+import { Error400, Error404 } from '../utils/customError.js';
 
 export const handleNotFound = (req, res) => {
     response.res404('Resource not found!', res);
@@ -8,8 +8,11 @@ export const handleNotFound = (req, res) => {
 export const handleOther = (err, req, res, next) => {
     if (err) {
         if (err instanceof Error400) {
-            console.log('Client error: ' + err.message);
+            console.log('Bad Request: ' + err.message);
             response.res400(err.message, res);
+        } else if(err instanceof Error404) {
+            console.log('Not Found: ' + err.message);
+            response.res404(err.message, res);
         } else {
             console.log('Server error: ' + err.message);
             response.res500(res);

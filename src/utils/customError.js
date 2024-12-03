6565b34ec4ac799b688f89baebf0c8fc79c93cb6
fajrCode <fajri42vandi@gmail.com>
2024-12-03 10:53:13 +0700

    chore: custom error | commnet custom error not use

--- src/utils/customError.js
+++ src/utils/customError.js
@@ -15,15 +15,15 @@ export class Error401 extends Error {
         this.name = 'Unauthorized';
     }
 }
-
-export class Error403 extends Error {
-    constructor(message, statusCode = 403) {
-        super(message);
-        this.status = false;
-        this.statusCode = statusCode;
-        this.name = 'Forbidden';
-    }
-}
+// Uncomment when needed
+// export class Error403 extends Error {
+//     constructor(message, statusCode = 403) {
+//         super(message);
+//         this.status = false;
+//         this.statusCode = statusCode;
+//         this.name = 'Forbidden';
+//     }
+// }
 
 export class Error404 extends Error {
     constructor(message, statusCode = 404) {
@@ -43,29 +43,30 @@ export class Error409 extends Error {
     }
 }
 
-export class Error500 extends Error {
-    constructor(message, statusCode = 500) {
-        super(message);
-        this.status = false;
-        this.statusCode = statusCode;
-        this.name = 'InternalServerError';
-    }
-}
+// Uncomment when needed
+// export class Error500 extends Error {
+//     constructor(message, statusCode = 500) {
+//         super(message);
+//         this.status = false;
+//         this.statusCode = statusCode;
+//         this.name = 'InternalServerError';
+//     }
+// }
 
-export class Error502 extends Error {
-    constructor(message, statusCode = 502) {
-        super(message);
-        this.status = false;
-        this.statusCode = statusCode;
-        this.name = 'BadGateway';
-    }
-}
+// export class Error502 extends Error {
+//     constructor(message, statusCode = 502) {
+//         super(message);
+//         this.status = false;
+//         this.statusCode = statusCode;
+//         this.name = 'BadGateway';
+//     }
+// }
 
-export class Error503 extends Error {
-    constructor(message, statusCode = 503) {
-        super(message);
-        this.status = false;
-        this.statusCode = statusCode;
-        this.name = 'ServiceUnavailable';
-    }
-}
+// export class Error503 extends Error {
+//     constructor(message, statusCode = 503) {
+//         super(message);
+//         this.status = false;
+//         this.statusCode = statusCode;
+//         this.name = 'ServiceUnavailable';
+//     }
+// }

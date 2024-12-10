159fffaa726ab2878fd9866e26a433f5e9369726
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-12-10 22:52:00 +0700

    Feat/payment v2 (#34)
    * add: new pay controller
    
    * update: berhasil create pembayaran
    
    * update : success check status payment
    
    * update : update status in database
    
    * update :add  metode payment
    
    * update : joi validation payment
    
    * add: unit testing payment
    
    * update : validation and error handling
    
    * update : git ignore
    
    * update : eslint
    
    * add : node mailer in payment
    
    * update : index.js
    
    * update : seeder
    
    ---------
    
    Co-authored-by: Fajri A <103434966+fajrCode@users.noreply.github.com>

--- src/routes/api/v1/payment.route.js
+++ src/routes/api/v1/payment.route.js
@@ -4,7 +4,7 @@ import * as paymentController from '../../../controllers/payment.controller.js';
 export default (router) => {
     const prefix = '/payments';
 
-    router.post(prefix + '/debit', paymentController.createDebitPayment);
+    router.post(prefix + '/virtual-account', paymentController.createDebitPayment);
     router.delete(prefix + '/:transactionId', paymentController.cancelPayment);
     router.get(prefix + '/:transactionId', paymentController.checkPaymentStatus);
     router.post(prefix + '/gopay', paymentController.createGoPayPayment);

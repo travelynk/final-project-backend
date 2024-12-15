f182d58631f40de3bdeb23e43f1c1fed80ee8ded
fajrCode <fajri42vandi@gmail.com>
2024-12-16 00:18:19 +0700

    feat: update flight validation and response get

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -3,7 +3,7 @@ export const formatTime = (utcDateStr) => {
     const jakartaTime = new Date(date.toLocaleString("en-US", { timeZone: "Asia/Jakarta" }));
 
     return {
-        date: `${String(jakartaTime.getDate()).padStart(2, "0")}-${String(jakartaTime.getMonth() + 1).padStart(2, "0")}-${jakartaTime.getFullYear()}`,
+        date: `${jakartaTime.getFullYear()}-${String(jakartaTime.getMonth() + 1).padStart(2, "0")}-${String(jakartaTime.getDate()).padStart(2, "0")}`,
         time: `${String(jakartaTime.getHours()).padStart(2, "0")}:${String(jakartaTime.getMinutes()).padStart(2, "0")}`,
     };
 };

b76da069988c34c2c1ab0913938b8a9797e4e8b0
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-12-22 12:56:51 +0700

    update : refactor payment service

--- src/utils/generateQrcode.js
+++ src/utils/generateQrcode.js
@@ -4,21 +4,6 @@ import { generateQrPng } from "./qrcode.js";
 import { imagekit } from "./imagekit.js";
 import prisma from "../configs/database.js";
 
-// export const generateQrCode = async (id) => {
-//     const code = await encodeBookingCode(id);
-//     const resetToken = jwt.sign({ code }, process.env.JWT_SECRET_FORGET);
-//     const url = `${process.env.DOMAIN_URL}/api/v1/bookings/ticket?token=${resetToken}`;
-
-//     const qr = await generateQrPng(url);
-
-//     const qrCode = await imagekit.upload({
-//         fileName: "booking_qr_code",
-//         file: qr.toString('base64')
-//     });
-
-//     return qrCode.url;
-// };
-
 export const generateQrCode = async (id) => {
     const code = await encodeBookingCode(id);
     const resetToken = jwt.sign({ code }, process.env.JWT_SECRET_FORGET);

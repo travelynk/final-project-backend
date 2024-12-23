703d87cdf9b293823a1492c90f800103447fb51b
fajrCode <fajri42vandi@gmail.com>
2024-12-23 17:43:46 +0700

    feat: flight | update endpoint available flight

--- src/services/flight.service.js
+++ src/services/flight.service.js
@@ -217,7 +217,6 @@ export const getAvailableFlight = async (data) => {
     const [depCity, arrCity] = route;
     let outboundFlights = [];
     let returnFlights = [];
-    const now = new Date();
 
     if (schedule.length > 1) {
         const returnStartDay = new Date(schedule[1]);
@@ -227,7 +226,7 @@ export const getAvailableFlight = async (data) => {
         const flights = await prisma.flight.findMany({
             where: {
                 departureTime: {
-                    gte: now,
+                    gte: returnStartDay,
                     lt: returnEndDay,
                 },
                 seatClass,
@@ -271,7 +270,7 @@ export const getAvailableFlight = async (data) => {
     const flights = await prisma.flight.findMany({
         where: {
             departureTime: {
-                gte: now,
+                gte: startDay,
                 lt: endDay,
             },
             seatClass,
